20e0598e9cfaf172e4c657e783fb013f168a1252
Bhagwat Kumar <bhagwat@intelligrape.com>
2014-10-31 17:30:08 +0530

    updated code to deomonstrate Resource annotation for DI

--- cli/injection.groovy
+++ cli/injection.groovy
@@ -1,20 +1,25 @@
 import org.springframework.beans.factory.annotation.Qualifier;
 import javax.annotation.Resource
 
-interface Animal{String name}
+interface Animal{public void saySomething()}
+
 @Service
-class Dog implements Animal{ void bark(){println "bark"}}
+class Dog implements Animal{ void saySomething(){println "bark"}}
+
 @Service
-class Cat implements Animal{void meow(){println "meow"}}
+class Cat implements Animal{void saySomething(){println "meow"}}
 
 @RestController
 class Main{
-	@Resource
-	@Qualifier("dog")
-	Animal animal
 
-	@RequestMapping("/")
-	public String hello(){
-		return "hello world"
-	}
+  @Resource
+  @Qualifier("dog")
+  Animal animal
+
+  @RequestMapping("/")
+  public String hello(){
+	animal.saySomething()
+
+	return "hello world"
+  }
 }
