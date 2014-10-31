7eb4d941512f405c693de0275824631fd99d67de
Bhagwat Kumar <bhagwat@intelligrape.com>
2014-10-31 17:29:31 +0530

    updated code to demonstrate spring IoC and DI using annotations

--- cli/service.groovy
+++ cli/service.groovy
@@ -1,22 +1,42 @@
-@Grab('spring-boot-starter-security')
 @Controller
 class Example {
+ @Autowired ServiceExample serviceExample
+ @Autowired ComponentExample componentExample
+ @Autowired RepositoryExample repositoryExample
+ @Autowired String appName
+ @Autowired String appVersion
 
-	@Autowired
-	private MyService myService;
-
-	@RequestMapping("/")
-	@ResponseBody
-	public String helloWorld() {
-		return myService.sayWorld();
-	}
-
+ @RequestMapping("/")
+ public String serve(){
+	println serviceExample.sayHello()
+	println componentExample.sayHello()
+	println repositoryExample.sayHello()
+	println appName
+	println appVersion
+	return "Request served. Check log...."
+ }
 }
 
 @Service
-class MyService {
+class ServiceExample {
+ public String sayHello(){"Hello from Service Example"}
+}
 
-	public String sayWorld() {
-		return "World!";
-	}
+@Component
+class ComponentExample{
+ public String sayHello(){"Hello from Component Example"}
+}
+
+@Repository
+class RepositoryExample{
+ public String sayHello(){"Hello from Repository example"}
+}
+
+@Configuration
+class BeanDefination{
+  @Bean
+  String appName(){return "Test app"}
+
+  @Bean(name = "appVersion")
+  String getVersion(){return "1.0.0"}
 }
